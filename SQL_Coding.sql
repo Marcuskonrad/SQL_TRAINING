ac53333b87eb6009973a4c5bd66b388aaa4e2ded
marc.kunz@students.fhnw.ch <marc.kunz@students.fhnw.ch>
2021-06-05 19:49:34 +0200

    updated new codes

--- SQL_Coding.sql
+++ SQL_Coding.sql
@@ -1,6 +1,10 @@
 /*SQL Coding for training with ORACAL "https://livesql.oracle.com/apex/f?p=590:1:11257469250555::NO:::"
-I use Oracal for training with SQL Code.
-*/
+I use Oracal for training with SQL Code. You can also find more informaion on der website https://www.w3schools.com/sql/sql_union.asp
+
+
+The SQL WHERE Clause
+The WHERE clause is used to filter records.
+It is used to extract only those records that fulfill a specified condition.*/
 
 
 --1 AND, IN, NOT EXISTS und OR Operator in the WHERE Clause
@@ -19,3 +23,16 @@ FROM HR.DEPARTMENTS  --HR.EMPLOYEES
 WHERE EXISTS (SELECT MANAGER_ID FROM HR.EMPLOYEES WHERE HR.EMPLOYEES.MANAGER_ID = HR.DEPARTMENTS.MANAGER_ID and SALARY < 17000);
 
 
+/*The SQL UNION Operator
+
+The UNION operator is used to combine the result-set of two or more SELECT statements.
+
+    Every SELECT statement within UNION must have the same number of columns
+    The columns must also have similar data types
+    The columns in every SELECT statement must also be in the same order*/
+
+--2 UNION, UNION ALL, MINUS, LIKE und INTERSECT Operatoren
+
+SELECT MANAGER_ID  FROM HR.EMPLOYEES
+UNION
+SELECT MANAGER_ID FROM HR.DEPARTMENTS
